8d3f82c4f9798c8b7b1f7cdf89969e429d00df24
Whyukim <110218594+Whyukim@users.noreply.github.com>
2023-04-02 12:00:00 +0900

    camera exam

---
+++ controls/src/ex02.js
@@ -0,0 +1,95 @@
+import * as THREE from "three";
+import { TrackballControls } from "three/examples/jsm/controls/TrackballControls";
+
+// ----- 주제: OrbitControls
+
+export default function example() {
+  // Renderer
+  const canvas = document.querySelector("#three-canvas");
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+
+  // Scene
+  const scene = new THREE.Scene();
+
+  // Camera
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.y = 1.5;
+  camera.position.z = 4;
+  scene.add(camera);
+
+  // Light
+  const ambientLight = new THREE.AmbientLight("white", 0.5);
+  scene.add(ambientLight);
+
+  const directionalLight = new THREE.DirectionalLight("white", 1);
+  directionalLight.position.x = 1;
+  directionalLight.position.z = 2;
+  scene.add(directionalLight);
+
+  // Controls
+  const controls = new TrackballControls(camera, renderer.domElement);
+
+  controls.enableDamping = true; // 부드러워짐
+  //   controls.enableZoom = false;  // zoom 불가
+  controls.maxDistance = 10; // 카메라 최대 줌 아웃
+  controls.minDistance = 2; // 카메라 최대 줌
+  controls.maxPolarAngle = THREE.MathUtils.degToRad(135); // 카메라 최대 각도
+  controls.minPolarAngle = THREE.MathUtils.degToRad(45); // 카메라 최소 각도
+  //   controls.target.set(2, 2, 2); // 중심
+  controls.autoRotate = true; // 자동 회전
+  controls.rotateSpeed = 5; // 회전속도
+
+  // Mesh
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  let mesh;
+  let material;
+  for (let i = 0; i < 20; i++) {
+    material = new THREE.MeshStandardMaterial({
+      color: `rgb(
+			${Math.floor(Math.random() * 255)},
+			${Math.floor(Math.random() * 255)},
+			${Math.floor(Math.random() * 255)}
+		)`,
+    });
+
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.x = (Math.random() - 0.5) * 5;
+    mesh.position.y = (Math.random() - 0.5) * 5;
+    mesh.position.z = (Math.random() - 0.5) * 5;
+
+    scene.add(mesh);
+  }
+
+  // 그리기
+  const clock = new THREE.Clock();
+
+  function draw() {
+    const delta = clock.getDelta();
+    controls.update();
+
+    renderer.render(scene, camera);
+    renderer.setAnimationLoop(draw);
+  }
+
+  function setSize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  // 이벤트
+  window.addEventListener("resize", setSize);
+
+  draw();
+}
